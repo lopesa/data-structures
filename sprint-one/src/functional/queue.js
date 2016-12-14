259e61d09677631e3d26fe2f954eb03be20597fa
lopesa <tony@lopesdesign.com>
2016-12-14 15:06:05 -0800

    Completed functional instantiation pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,33 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var storageVal;
+  var increment = 0;
+  var addSize = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[addSize] = value;
+    addSize ++;
+    size ++;
   };
 
   someInstance.dequeue = function() {
+    
+    size --;
+    storageVal = storage[increment];
+    delete storage[increment];
+    increment ++;
+    return storageVal;
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      size = 0;
+    }
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var savedVal;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    size--;
+    savedVal = storage[size];
+    delete storage[size];
+    return savedVal;
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      size = 0;
+    }
+    return size;
   };
 
   return someInstance;
