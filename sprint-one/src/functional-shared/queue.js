96b1785e71b73d62995ebb303a24aaf0f28bd31b
lopesa <tony@lopesdesign.com>
2016-12-14 15:49:38 -0800

    Completed Functional-shared Instantiation Pattern for Stack and Queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instantiation = {};
+
+  _.extend(instantiation, queueMethods);
+
+  instantiation.objSize = 0;
+  instantiation.increment = 0;
+  instantiation.addInc = 0;
+  instantiation.storage = {};
+  instantiation.tempSto;
+  return instantiation;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.objSize ++;
+    this.storage[this.addInc] = value;
+    this.addInc++;
+  },
+  dequeue: function() {
+    this.objSize --;
+    this.tempSto = this.storage[this.increment];
+    delete this.storage[this.increment];
+    this.increment ++;
+    return this.tempSto;
+  },
+  size: function() {
+    if (this.objSize < 0) {
+      this.objSize = 0;
+    }
+    return this.objSize;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstantiation = {};
+  newInstantiation.newStorage = {};
+  newInstantiation.temp;
+  newInstantiation.instanceSize = 0;
+  _.extend(newInstantiation, stackMethods);
+  return newInstantiation;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.newStorage[this.instanceSize] = value;
+    this.instanceSize++;
+  },
+
+  pop: function() {
+    this.instanceSize--;
+    this.temp = this.newStorage[this.instanceSize];
+    delete this.newStorage[this.instanceSize];
+    return this.temp;
+
+  },
+
+  size: function() {
+    if (this.instanceSize < 0) {
+      this.instanceSize = 0;
+    }
+    return this.instanceSize;
+  }
+
+};
 
 
