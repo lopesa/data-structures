c9f1b15e3c691602cbdb53312b7a24b231032c30
lopesa <tony@lopesdesign.com>
2016-12-14 18:27:40 -0800

    Progress with Prototypial instantiation

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+Stack.prototype.push = function() {
+
+};
+
+Stack.prototype.pop = function() {
+
+};
+
+// Stack.prototype.size = function() {
+//   return 0;
+
+// };
+
+var stackMethods = {
+
+  size: function() {   return 0;}
+};
 
 
